866e8335d34ba6a3da71b4572e79d54c02ae2073
Dekel Matalon <dekel2003@gmail.com>
2017-05-16 20:21:17 +0300

    completed q2 and added well cond. and ill cond. functions

--- hw2/gradient_descent.m
+++ hw2/gradient_descent.m
@@ -1,14 +1,22 @@
-function [ min_x, min_val ] = gradient_descent( f, grad_f, x0, alpha0, beta, sigma, epsilon )
+function [ min_x, min_val ] = gradient_descent( f, grad_f, x0, alpha0, beta, sigma, epsilon, optimal_val )
     
 
 x = x0;
 alpha = alpha0;
-while (norm(grad_f(x)) >= epsilon)
+iteration = 0;
+while (sqrt(sum(grad_f(x).^2)) >= epsilon)
+    iteration = iteration+1;
     alpha = armijo(f, -(grad_f(x)), x, alpha, beta, sigma);
     x = x - alpha * (grad_f(x));
-    disp(norm(f(x)));
+    disp(norm(grad_f(x)));
     disp([grad_f(x) x]);
+    values(iteration) = f(x) - f(optimal_val);
 end
+
+semilogy(values)
+title('gradient descent');
+xlabel('iteration') % x-axis label
+ylabel('f(x)-p*') % y-axis label
 
 min_x = x;
 
@@ -19,14 +27,11 @@ end
 end
 
 
-
-
-
 function alpha_new = armijo(f, grad_f_x, x, alpha, beta, sigma)
-    m = sum(grad_f_x .* normc(grad_f_x));
+    m = -grad_f_x' * normc(grad_f_x);
     t = -sigma * m;
     alpha_new = alpha;
-    while (f(x) - f(x + alpha_new*grad_f_x)) < alpha_new*t
+    while (f(x + alpha_new*grad_f_x) - f(x)) > alpha_new*t
         alpha_new = alpha_new * beta;
     end
 end
--- hw2/q2.m
+++ hw2/q2.m
@@ -1,18 +1,21 @@
 
-
+alpha0 = 1;
 beta = 0.5;
 sigma = 0.25;
 epsilon = 10e-5;
 
 %%  Performing Gradient Descent on Rosenbrock function
-alpha0 = 1;
 
 N=10;
 f_1 = @(x) sum((1-x(1:end-1)).^2 +100*(x(2:end)-x(1:end-1).^2).^2);
 grad_f1 = @(x) [(-2*(1-x(1:end-1)) -400*x(1:end-1).*(x(2:end)-x(1:end-1).^2)) ;0] +200*([0;(x(2:end)-x(1:end-1).^2)]);
+H_f1 = @(x) zeros(N) + ...
+    diag( [2-400*(x(2:end)-x(1:end-1).^2)+800*x(1:end-1).^2;0] + [0;200*ones(N-1,1)] ) + ...
+    diag( [-400*x(2:end-1) ;0], 1) + ...
+    diag( [-400*x(1:end-2) ;0],-1)      ;
 x0 = zeros(N,1);
 optimal_val = ones(N,1);
-gradient_descent( f_1, grad_f1, x0, alpha0, beta, sigma, epsilon, optimal_val )
+newton_method( f_1, grad_f1, H_f1, x0, alpha0, beta, sigma, epsilon, optimal_val )
 
 %% well conditioned function
 alpha0 = pi/10;
@@ -22,9 +25,10 @@ b = ones(N,1)*-4;
 c = 4*N;
 f2 = @(x) x'*A*x + b'*x + c;
 g2 = @(x) 2*A*x + b;
+H2 = @(x) 2*A;
 x0 = (1:N)';
 optimal_val = ones(N,1)*2; optimal_val(1)=1;
-gradient_descent( f2, g2, x0, alpha0, beta, sigma, epsilon, optimal_val )
+newton_method( f2, g2, H2, x0, alpha0, beta, sigma, epsilon, optimal_val )
 
 %% ill conditioned function
 alpha0 = pi/10;
@@ -34,6 +38,7 @@ b = ones(N,1)*-4;
 c = 4*N;
 f3 = @(x) x'*A*x + b'*x + c;
 g3 = @(x) 2*A*x + b;
+H3 = @(x) 2*A;
 x0 = (1:N)';
 optimal_val = ones(N,1)*2; optimal_val(1)=0.02; optimal_val(2)=200;
-gradient_descent( f3, g3, x0, alpha0, beta, sigma, epsilon, optimal_val )
+newton_method( f3, g3, H3, x0, alpha0, beta, sigma, epsilon, optimal_val )
